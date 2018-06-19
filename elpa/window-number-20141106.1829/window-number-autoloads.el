8f5151a143cdc1f749bb59e0e69507b001efc18a
c4371143 <manuel.schneckenreither@uibk.ac.at>
2018-06-19 22:28:41 +0200

    26

---
+++ elpa/window-number-20141106.1829/window-number-autoloads.el
@@ -0,0 +1,16 @@
+;;; window-number-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("window-number.el") (22455 28330 936494
+;;;;;;  715000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; window-number-autoloads.el ends here
