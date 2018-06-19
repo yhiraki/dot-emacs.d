d6264d3f8c1dd19bb2e0610666fa536d7b3ffa27
c4371143 <manuel.schneckenreither@uibk.ac.at>
2018-06-19 22:56:52 +0200

    fix sh / shell babel load

---
+++ elpa/ac-octave-20180405.2034/ac-octave-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ac-octave" "20180405.2034" "An auto-complete source for Octave" '((auto-complete "1.4.0")) :commit "fe0f931f2024f43de3c4fff4b1ace672413adeae" :keywords '("octave" "auto-complete" "completion") :url "https://github.com/coldnew/ac-octave")
--- elpa/flyspell-correct-helm-20161031.1134/flyspell-correct-helm-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; flyspell-correct-helm-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("flyspell-correct-helm.el") (22604 30783
-;;;;;;  588480 202000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; flyspell-correct-helm-autoloads.el ends here
