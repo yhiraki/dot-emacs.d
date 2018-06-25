1504b98506a4d9fe03175ee22e9954723e5ab8b7
Manuel Schneckenreither <manuel.schneckenreither@student.uibk.ac.at>
2018-06-25 17:11:16 +0200

    added subsubsection

--- 25/packages_configs/org_mode_config.el
+++ 25/packages_configs/org_mode_config.el
@@ -316,9 +316,9 @@
                [DEFAULT-PACKAGES]
                [PACKAGES]
                [EXTRA]"
-               ("\\chapter{%s}" . "\\chapter*{%s}")
                ("\\section{%s}" . "\\section*{%s}")
                ("\\subsection{%s}" . "\\subsection*{%s}")
+               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
                ("\\paragraph{%s}" . "\\paragraph*{%s}")
                ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
 
