76f0f0cc20878c8d7add77e9cadf043e8047fbfc
Manuel Schneckenreither <manuel.schneckenreither@student.uibk.ac.at>
2018-07-02 11:19:28 +0200

    helm-M-x for M-x

--- 25/packages_configs/helm_config.el
+++ 25/packages_configs/helm_config.el
@@ -28,6 +28,8 @@
 (global-set-key (kbd "C-x r b")                      'helm-filtered-bookmarks)
 (global-set-key (kbd "C-x C-f")                      'helm-find-files)
 (global-set-key (kbd "C-x C-m")                      'helm-M-x)
+(global-set-key (kbd "M-x")                          'undefined)
+(global-set-key (kbd "M-x")                          'helm-M-x)
 
 
 (when (executable-find "curl")
